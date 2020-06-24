f9650b61a5337a6b55a1ddef0240f4ebec9d75dc
Wender Machado <wender.machado@rgsistemas.com.br>
2020-06-23 23:33:00 -0300

    :sparkles: Added PipedriveModule and BlingModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { MongooseConfigService } from './config/mongoose-config.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PipedriveModule } from './pipedrive/pipedrive.module';
+import { BlingModule } from './bling/bling.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRootAsync({ useClass: MongooseConfigService }),
+    PipedriveModule,
+    BlingModule,
   ],
   controllers: [
     AppController,
